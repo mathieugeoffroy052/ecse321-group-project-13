c0ab5928360a8f578c36676456749f782e263868
amanij <amani.jammoul@mail.mcgill.ca>
2021-10-12 13:09:28 -0400

    Initial commit of backend application

---
+++ LibraryService-Backend/src/test/java/ca/mcgill/ecse321/libraryservice/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
