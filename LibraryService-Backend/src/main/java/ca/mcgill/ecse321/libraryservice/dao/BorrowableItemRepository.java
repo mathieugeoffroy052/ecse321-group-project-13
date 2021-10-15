7ec69b4d139f022250f6dd49a2daa74a5e5221c8
amanij <amani.jammoul@mail.mcgill.ca>
2021-10-15 14:17:17 -0400

    CRUD methods for BorrowableItem repository

--- LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/BorrowableItemRepository.java
+++ LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/BorrowableItemRepository.java
@@ -5,10 +5,16 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.libraryservice.model.BorrowableItem;
+import ca.mcgill.ecse321.libraryservice.model.LibraryItem;
+import ca.mcgill.ecse321.libraryservice.model.UserAccount;
 
 public interface BorrowableItemRepository extends CrudRepository<BorrowableItem, Integer>{
 	
 	BorrowableItem findBorrowableItemByBarCodeNumber(int barCodeNumber);
 	
-	List<BorrowableItem> findByLibraryItem(int isbn);
+	List<BorrowableItem> findByLibraryItem(LibraryItem libraryItem);
+	
+	List<BorrowableItem> findByBorrower(UserAccount borrower);
+	
+	List<BorrowableItem> findByReserver(UserAccount reserver);
 }
