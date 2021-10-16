ad0242b3b435f4d0f25085e5c8b4f65724dbc6a6
amanij <amani.jammoul@mail.mcgill.ca>
2021-10-16 17:17:37 -0400

    remove unused imports

--- LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/BorrowableItemRepository.java
+++ LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/BorrowableItemRepository.java
@@ -1,7 +1,5 @@
 package ca.mcgill.ecse321.libraryservice.dao;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.libraryservice.model.BorrowableItem;
--- LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/PersonRepository.java
+++ LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/PersonRepository.java
@@ -1,12 +1,7 @@
 package ca.mcgill.ecse321.libraryservice.dao;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
-import ca.mcgill.ecse321.libraryservice.model.Book;
-import ca.mcgill.ecse321.libraryservice.model.Movie;
-import ca.mcgill.ecse321.libraryservice.model.Music;
 import ca.mcgill.ecse321.libraryservice.model.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Integer>{
