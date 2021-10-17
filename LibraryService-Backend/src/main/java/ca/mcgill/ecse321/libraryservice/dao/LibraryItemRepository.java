ba6da7acc2c30dceaf71cbc91adfead4c5fbe69b
amanij <amani.jammoul@mail.mcgill.ca>
2021-10-16 23:24:11 -0400

    CRUD method, return list of LibraryItems for a LibrarySystem

--- LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/LibraryItemRepository.java
+++ LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/LibraryItemRepository.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse321.libraryservice.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.libraryservice.model.LibraryItem;
+import ca.mcgill.ecse321.libraryservice.model.LibrarySystem;
 
 public interface LibraryItemRepository extends CrudRepository<LibraryItem, Integer>{
     
     LibraryItem findByIsbn(int isbn);
 
+    List<LibraryItem> findByLibrarySystem(LibrarySystem librarySystem);
 }
