0132713319b11691153b980b378466fc54b3378c
Zoya Malhi <zmalhi@mail.mcgill.ca>
2021-10-16 20:06:40 -0400

    Added method to RoomRepository

--- LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/RoomRepository.java
+++ LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/dao/RoomRepository.java
@@ -2,8 +2,11 @@ package ca.mcgill.ecse321.libraryservice.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
+import ca.mcgill.ecse321.libraryservice.model.Patron;
 import ca.mcgill.ecse321.libraryservice.model.Room;
 
 public interface RoomRepository extends CrudRepository<Room, Integer>{
 
+	
+	Room findRoomByIsbn(int isbn);
 }
