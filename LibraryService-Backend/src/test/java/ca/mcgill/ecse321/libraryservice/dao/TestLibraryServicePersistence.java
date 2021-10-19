fd8b807ea45808d36b4dd8de19c5af0d7246f695
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2021-10-19 00:01:10 -0400

    fixed bug

--- LibraryService-Backend/src/test/java/ca/mcgill/ecse321/libraryservice/dao/TestLibraryServicePersistence.java
+++ LibraryService-Backend/src/test/java/ca/mcgill/ecse321/libraryservice/dao/TestLibraryServicePersistence.java
@@ -744,7 +744,7 @@ public class TestLibraryServicePersistence {
         //test library item within transaction
         assertEquals(name, transaction.getBorrowableItem().getLibraryItem().getName(), "transaction.borrowableItem.state mismatch");
         assertEquals(author, ((Book) transaction.getBorrowableItem().getLibraryItem()).getAuthor(), "transaction.borrowableitem.libraryItem.author mismatch"); 
-        assertEquals(library.getSystemId(), transaction.getBorrowableItem().getLibraryItem().getLibrarySystem(), "transaction.borrowableitem.libraryItem.librarySystem.systemID mismatch");
+        assertEquals(library.getSystemId(), transaction.getBorrowableItem().getLibraryItem().getLibrarySystem().getSystemId(), "transaction.borrowableitem.libraryItem.librarySystem.systemID mismatch");
 
     }
 
