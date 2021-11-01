53ecdcb2c49aa91cac1c30c1629c41cbf858b188
GabbyHalpin <gabriellehalpin@icloud.com>
2021-11-01 19:42:28 -0400

    first service methods for the creation of patron and librarian accounts

--- LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/service/LibraryServiceService.java
+++ LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/service/LibraryServiceService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ca.mcgill.ecse321.libraryservice.dao.*;
+import ca.mcgill.ecse321.libraryservice.model.*;
 
 
 @Service
@@ -39,4 +40,45 @@ public class LibraryServiceService {
     @Autowired
     private UserAccountRepository userAccountRepository;
 	
+    @Transactional
+	public Librarian createLibrarian(String aFirstName, String aLastName, boolean aOnlineAccount, LibrarySystem aLibrarySystem, String aAddress, String aPassword, int aBalance) {
+		Librarian librarian = new Librarian();
+		librarian.setFirstName(aFirstName);
+        librarian.setLastName(aLastName);
+        librarian.setOnlineAccount(aOnlineAccount);
+        librarian.setLibrarySystem(aLibrarySystem);
+        librarian.setAddress(aAddress);
+        librarian.setPassword(aPassword);
+        librarian.setBalance(aBalance);
+		librarianRepository.save(librarian);
+		return librarian;
+	}
+
+    @Transactional
+	public UserAccount getUser(int userID) {
+		UserAccount person = userAccountRepository.findUserAccountByUserID(userID);
+		return person;
+	}
+
+    @Transactional
+	public Patron createPatron(String aFirstName, String aLastName, boolean aOnlineAccount, LibrarySystem aLibrarySystem, String aAddress, boolean aValidatedAccount, String aPassword, int aBalance) {
+		Patron patron = new Patron();
+		patron.setFirstName(aFirstName);
+        patron.setLastName(aLastName);
+        patron.setOnlineAccount(aOnlineAccount);
+        patron.setLibrarySystem(aLibrarySystem);
+        patron.setAddress(aAddress);
+        patron.setPassword(aPassword);
+        patron.setBalance(aBalance);
+        patron.setValidatedAccount(aValidatedAccount);
+		patronRepository.save(patron);
+		return patron;
+	}
+
+    @Transactional
+	public Patron getPatron(int userID) {
+		Patron person = patronRepository.findPatronByUserID(userID);
+		return person;
+	}
+
 }
