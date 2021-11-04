0a4d8f739006d9349749adaea40a0ab9abb687a3
rakhavan55 <ramin.akhavan@mail.mcgill.ca>
2021-11-04 11:41:13 -0400

    Re-added the convert dto methods that I made

--- LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/controller/LibraryServiceRestController.java
+++ LibraryService-Backend/src/main/java/ca/mcgill/ecse321/libraryservice/controller/LibraryServiceRestController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import ca.mcgill.ecse321.libraryservice.dto.LibrarianDTO;
-import ca.mcgill.ecse321.libraryservice.dto.PatronDTO;
-import ca.mcgill.ecse321.libraryservice.dto.UserAccountDTO;
+import ca.mcgill.ecse321.libraryservice.dto.*;
 import ca.mcgill.ecse321.libraryservice.model.*;
 import ca.mcgill.ecse321.libraryservice.service.LibraryServiceService;
 
@@ -48,6 +46,42 @@ public class LibraryServiceRestController {
         }
         LibrarianDTO librarianDTO = new LibrarianDTO(librarian.getFirstName(),librarian.getLastName(),librarian.getOnlineAccount(),librarian.getAddress(), librarian.getPassword(), librarian.getBalance());
         return librarianDTO;
+    }
+
+    private HeadLibrarianDTO convertToDto(HeadLibrarian headLibrarian) {
+        if (headLibrarian== null) {
+            throw new IllegalArgumentException("There is no such head librarian!");
+        }
+        HeadLibrarianDTO headLibrarianDTO = new HeadLibrarianDTO(headLibrarian.getFirstName(),headLibrarian.getLastName(),headLibrarian.getOnlineAccount(),headLibrarian.getAddress(), headLibrarian.getPassword(), headLibrarian.getBalance());
+        return headLibrarianDTO;
+    }
+
+    private HolidayDTO convertToDto(Holiday holiday, HeadLibrarian headLibrarian) {
+        if (holiday == null) {
+            throw new IllegalArgumentException("There is no such holiday!");
+        }
+        HeadLibrarianDTO headLibrarianDTO = new HeadLibrarianDTO(headLibrarian.getFirstName(),headLibrarian.getLastName(),headLibrarian.getOnlineAccount(),headLibrarian.getAddress(), headLibrarian.getPassword(), headLibrarian.getBalance());
+        HolidayDTO holidayDTO = new HolidayDTO(holiday.getDate(), holiday.getStartTime(), holiday.getEndtime(), headLibrarianDTO);
+        return holidayDTO;
+    }
+
+    private OpeningHourDTO convertToDto(OpeningHour openingHour, HeadLibrarian headLibrarian) {
+        if (openingHour == null) {
+            throw new IllegalArgumentException("There is no such opening hour!");
+        }
+        HeadLibrarianDTO headLibrarianDTO = new HeadLibrarianDTO(headLibrarian.getFirstName(),headLibrarian.getLastName(),headLibrarian.getOnlineAccount(),headLibrarian.getAddress(), headLibrarian.getPassword(), headLibrarian.getBalance());
+        OpeningHourDTO.DayOfWeek dayOfWeek = OpeningHourDTO.DayOfWeek.valueOf(openingHour.getDayOfWeek().toString());
+        OpeningHourDTO openingHourDTO = new OpeningHourDTO(dayOfWeek, openingHour.getStartTime(), openingHour.getEndTime(), headLibrarianDTO);
+        return openingHourDTO;
+    }
+
+    private LibraryItemDTO convertToDto(LibraryItem libraryItem) {
+        if (libraryItem== null) {
+            throw new IllegalArgumentException("There is no such library item!");
+        }
+        LibraryItemDTO.ItemType itemType = LibraryItemDTO.ItemType.valueOf(libraryItem.getType().toString());
+        LibraryItemDTO libraryItemDTO = new LibraryItemDTO(libraryItem.getName(), itemType, libraryItem.getDate(), libraryItem.getCreator(), libraryItem.getIsViewable());
+        return libraryItemDTO;
     }
 
 }
