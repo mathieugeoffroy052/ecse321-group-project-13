0cf01f79cd6f625fa1a2159beba550ee103dae35
rakhavan55 <ramin.akhavan@mail.mcgill.ca>
2021-11-09 18:39:58 -0500

    Removed library system repo unit test mock

--- LibraryService-Backend/src/test/java/ca/mcgill/ecse321/libraryservice/service/TestLibraryServiceService.java
+++ LibraryService-Backend/src/test/java/ca/mcgill/ecse321/libraryservice/service/TestLibraryServiceService.java
@@ -39,13 +39,9 @@ public class TestLibraryServiceService {
 	@Mock
 	private LibraryItemRepository libraryItemDao;
 
-	@Mock
-	private LibrarySystemRepository librarySystemDao;
 
 	@InjectMocks
 	private LibraryServiceService service;
-	
-	private LibrarySystem libSystem;
 	
 	private static final int LIBRARY_ITEM_ISBN = 123;
 	private static final String LIBRARY_ITEM_CREATOR = "Jeff Joseph";
@@ -54,21 +50,9 @@ public class TestLibraryServiceService {
 	private static final Date LIBRARY_ITEM_DATE = Date.valueOf("2009-03-15");
 	private static final boolean LIBRARY_ITEM_IS_VIEWABLE = false;
 	
-	private static final int LIBRARY_SYSTEM_ID = 0;
 
 	@BeforeEach
 	public void setMockOutput() {
-		lenient().when(librarySystemDao.findBySystemId(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
-			if (invocation.getArgument(0).equals(LIBRARY_SYSTEM_ID)) {
-				System.err.println("hello");
-				LibrarySystem librarySystem = new LibrarySystem();
-				librarySystem.setSystemId(LIBRARY_SYSTEM_ID);
-				this.libSystem = librarySystem;
-				return librarySystem;
-			} else {
-				return null;
-			}
-		});
 		lenient().when(libraryItemDao.findByIsbn(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
 			if (invocation.getArgument(0).equals(LIBRARY_ITEM_ISBN)) {
 				LibraryItem libraryItem = new LibraryItem();
@@ -78,7 +62,6 @@ public class TestLibraryServiceService {
 				libraryItem.setName(LIBRARY_ITEM_NAME);
 				libraryItem.setType(LIBRARY_ITEM_TYPE);
 				libraryItem.setIsViewable(LIBRARY_ITEM_IS_VIEWABLE);
-				libraryItem.setLibrarySystem(libSystem);
 				return libraryItem;
 			} else {
 				return null;
@@ -89,16 +72,15 @@ public class TestLibraryServiceService {
 			return invocation.getArgument(0);
 		};
 		lenient().when(libraryItemDao.save(any(LibraryItem.class))).thenAnswer(returnParameterAsAnswer);
-		lenient().when(librarySystemDao.save(any(LibrarySystem.class))).thenAnswer(returnParameterAsAnswer);
 	}
 
 	@Test
 	public void testGetBookFromTitle() {
 		try {
 			assertEquals(LIBRARY_ITEM_NAME, service.getBookFromTitle(LIBRARY_ITEM_NAME).getName());
-			assertEquals(true, false);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			assertEquals(false, true);
 			e.printStackTrace();
 			System.err.print("Could not get book!");
 		}
