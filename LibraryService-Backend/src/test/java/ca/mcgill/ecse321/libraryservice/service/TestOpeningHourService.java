baade43741b14624844c3a7657fd8c1645505ffe
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2021-11-11 18:00:34 -0500

    removed unused import from opening hour service tests

--- LibraryService-Backend/src/test/java/ca/mcgill/ecse321/libraryservice/service/TestOpeningHourService.java
+++ LibraryService-Backend/src/test/java/ca/mcgill/ecse321/libraryservice/service/TestOpeningHourService.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
 
 import java.sql.Time;
