c66ad3cb21eed344cd4e3bd03da0a5fd37206416
eloyann7 <eloyann.roy-javanbakht@mail.mcgill.ca>
2021-11-23 22:11:45 -0500

    style added

--- LibraryService-Frontend/src/components/Login.vue
+++ LibraryService-Frontend/src/components/Login.vue
@@ -24,8 +24,60 @@
 
 <style>
   #eventregistration {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background: #f2ece8;
+    position: absolute;
+    width: 1433px;
+    height: 831px;
+
   }
 </style>
+
+<style>
+  #libraryTitle {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+
+    position: absolute;
+    width: 540px;
+    height: 98px;
+    left: 152px;
+    top: 59px;
+  }
+  </style>
+
+  <style>
+  #lightblueBox {
+    position: absolute;
+    width: 519px;
+    height: 414px;
+    left: 151px;
+    top: 294px;
+
+    background: rgba(255, 255, 255, 0.6);
+    border: 1px solid #000000;
+    box-sizing: border-box;
+  }
+  </style>
+
+  <style>
+  #whiteseperator {
+  position: absolute;
+  width: 674px;
+  height: 866px;
+  left: 766px;
+  top: 0px;
+  }
+  </style>
+
+  <style>
+  #logoBook {
+    position: absolute;
+width: 115.43px;
+height: 114.04px;
+left: calc(50% - 115.43px/2 - 641.26px);
+top: calc(50% - 114.04px/2 - 353.48px);
+
+transform: rotate(0.48deg);
+  }
+  </style>
