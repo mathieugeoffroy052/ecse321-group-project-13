79f18dacd9cfc74b07e9d05b5e878385e1aa41a0
amanij <amani.jammoul@mail.mcgill.ca>
2021-11-23 23:29:05 -0500

    create itemBrowse comp. & add book image

--- LibraryService-Frontend/src/router/index.js
+++ LibraryService-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import ItemTypeToBrowse from '@/components/ItemTypeToBrowse'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/item-select',
+      name: 'ItemTypeToBrowse',
+      component: ItemTypeToBrowse
     }
   ]
 })
