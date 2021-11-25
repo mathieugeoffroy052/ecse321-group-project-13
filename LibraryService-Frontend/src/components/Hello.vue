3cd5a5b9a875086fdda63ea7027cfe4f388319e8
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2021-11-25 14:03:02 -0500

    Small changes

--- LibraryService-Frontend/src/components/Hello.vue
+++ LibraryService-Frontend/src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="librarySystem">
-    <h2>Hello</h2>
+    <h2>Login</h2>
     <table>
       <tr>
           <!-- <td>UserId</td>
@@ -8,16 +8,16 @@
       </tr>
       <tr>
           <td>
-              <input type="text" placeholder="Enter UserID">
+              <input type="text" placeholder="Enter UserID"> 
+                {text-align: center;}
               <input type="text" placeholder="Enter Password">
+                
           </td>
           <td>
               <button>Login</button>
-               
-                <div style="position:absolute; right:0px; top:0px;">
+              <div style="position:absolute; right:0px; top:0px;">
                   <button>Create an Account</button>
                 </div>
-
           </td>
       </tr>
     </table>
@@ -29,9 +29,59 @@
 
 <style>
   #librarySystem {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background: #f2ece8;
+    position: absolute;
+    width: 1433px;
+    height: 831px;
+
   }
-   
 </style>
+<style>
+  #libraryTitle {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+
+    position: absolute;
+    width: 540px;
+    height: 98px;
+    left: 152px;
+    top: 59px;
+  }
+  </style>
+
+  <style>
+  #lightblueBox {
+    position: absolute;
+    width: 519px;
+    height: 414px;
+    left: 151px;
+    top: 294px;
+
+    background: rgba(255, 255, 255, 0.6);
+    border: 1px solid #000000;
+    box-sizing: border-box;
+  }
+  </style>
+
+  <style>
+  #whiteseperator {
+  position: absolute;
+  width: 674px;
+  height: 866px;
+  left: 766px;
+  top: 0px;
+  }
+  </style>
+
+  <style>
+  #logoBook {
+    position: absolute;
+width: 115.43px;
+height: 114.04px;
+left: calc(50% - 115.43px/2 - 641.26px);
+top: calc(50% - 114.04px/2 - 353.48px);
+
+transform: rotate(0.48deg);
+  }
+  </style>
