a2de75dc24a91c30a648fcb61cfc6271f1afdea6
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2021-11-25 18:59:44 -0500

    Fixed up login page

--- LibraryService-Frontend/src/components/Hello.vue
+++ LibraryService-Frontend/src/components/Hello.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="librarySystem">
-    <h2>Login</h2>
+  <div id="Hello">
+    <div style="position:absolute; right:0px; left:0px; top:0px;">
+        <h2>Login</h2>
+      </div>
+    
     <table>
-      <tr>
-          <!-- <td>UserId</td>
-          <td>Login</td> -->
-      </tr>
+      
       <tr>
           <td>
               <input type="text" placeholder="Enter UserID"> 
@@ -45,43 +45,43 @@
     position: absolute;
     width: 540px;
     height: 98px;
-    left: 152px;
+    left: 50px;
     top: 59px;
   }
   </style>
 
   <style>
-  #lightblueBox {
-    position: absolute;
-    width: 519px;
-    height: 414px;
-    left: 151px;
-    top: 294px;
+    #lightblueBox {
+      position: absolute;
+      width: 519px;
+      height: 414px;
+      left: 151px;
+      top: 294px;
 
-    background: rgba(255, 255, 255, 0.6);
-    border: 1px solid #000000;
-    box-sizing: border-box;
-  }
+      background: rgba(255, 255, 255, 0.6);
+      border: 1px solid #000000;
+      box-sizing: border-box;
+    }
   </style>
 
   <style>
-  #whiteseperator {
-  position: absolute;
-  width: 674px;
-  height: 866px;
-  left: 766px;
-  top: 0px;
-  }
+    #whiteseperator {
+      position: absolute;
+      width: 674px;
+      height: 866px;
+      left: 766px;
+      top: 0px;
+    }
   </style>
 
   <style>
-  #logoBook {
-    position: absolute;
-width: 115.43px;
-height: 114.04px;
-left: calc(50% - 115.43px/2 - 641.26px);
-top: calc(50% - 114.04px/2 - 353.48px);
+    #logoBook {
+      position: absolute;
+      width: 115.43px;
+      height: 114.04px;
+      left: calc(50% - 115.43px/2 - 641.26px);
+      top: calc(50% - 114.04px/2 - 353.48px);
 
-transform: rotate(0.48deg);
-  }
+      transform: rotate(0.48deg);
+    }
   </style>
--- LibraryService-Frontend/src/components/Login.vue
+++ LibraryService-Frontend/src/components/Login.vue
@@ -1,6 +1,9 @@
 <template>
-  <div id="librarySystem">
-    <h2>Login</h2>
+  <div id="Login">
+    <div style="position:absolute; right:0px; left:0px; top:0px;">
+        <h2>Login</h2>
+      </div>
+    
     <table>
       <tr>
           <!-- <td>UserId</td>
@@ -8,16 +11,16 @@
       </tr>
       <tr>
           <td>
-              <input type="text" placeholder="Enter UserID">
+              <input type="text" placeholder="Enter UserID"> 
+               
               <input type="text" placeholder="Enter Password">
+                
           </td>
           <td>
               <button>Login</button>
-              <button>Create an Account</button> {
-                  position: absolute;
-                  top: 0px;
-                  right: 0px;
-              }
+              <div style="position:absolute; right:200px; top:-70px;">
+                  <button>Create an Account</button>
+                </div>
           </td>
       </tr>
     </table>
@@ -28,14 +31,13 @@
 </template>
 
 <style>
-  #eventregistration {
+  #librarySystem {
     position: absolute;
     width: 1433px;
     height: 831px;
 
   }
 </style>
-
 <style>
   #libraryTitle {
     display: flex;
@@ -46,43 +48,43 @@
     position: absolute;
     width: 540px;
     height: 98px;
-    left: 152px;
+    left: 50px;
     top: 59px;
   }
   </style>
 
   <style>
-  #lightblueBox {
-    position: absolute;
-    width: 519px;
-    height: 414px;
-    left: 151px;
-    top: 294px;
+    #lightblueBox {
+      position: absolute;
+      width: 519px;
+      height: 414px;
+      left: 151px;
+      top: 294px;
 
-    background: rgba(255, 255, 255, 0.6);
-    border: 1px solid #000000;
-    box-sizing: border-box;
-  }
+      background: rgba(255, 255, 255, 0.6);
+      border: 1px solid #000000;
+      box-sizing: border-box;
+    }
   </style>
 
   <style>
-  #whiteseperator {
-  position: absolute;
-  width: 674px;
-  height: 866px;
-  left: 766px;
-  top: 0px;
-  }
+    #whiteseperator {
+      position: absolute;
+      width: 674px;
+      height: 866px;
+      left: 766px;
+      top: 0px;
+    }
   </style>
 
   <style>
-  #logoBook {
-    position: absolute;
-width: 115.43px;
-height: 114.04px;
-left: calc(50% - 115.43px/2 - 641.26px);
-top: calc(50% - 114.04px/2 - 353.48px);
+    #logoBook {
+      position: absolute;
+      width: 115.43px;
+      height: 114.04px;
+      left: calc(50% - 115.43px/2 - 641.26px);
+      top: calc(50% - 114.04px/2 - 353.48px);
 
-transform: rotate(0.48deg);
-  }
+      transform: rotate(0.48deg);
+    }
   </style>
