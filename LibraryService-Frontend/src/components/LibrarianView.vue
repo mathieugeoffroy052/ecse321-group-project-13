1cc344777ee8a0c6d1f3d325e45a527866abd577
GabbyHalpin <gabriellehalpin@icloud.com>
2021-11-26 14:27:11 -0500

    these changes

--- LibraryService-Frontend/src/components/LibrarianView.vue
+++ LibraryService-Frontend/src/components/LibrarianView.vue
@@ -4,7 +4,53 @@
 <div>
   <b-tabs content-class="mt-3">
     <b-tab title="Patron Account" active><p>I'm the first tab</p></b-tab>
-    <b-tab title="Add Patron"><p>I'm the second tab</p></b-tab>
+    <b-tab title="Add User">
+        <div>
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+
+            <b-form-group id="input-group-1" label="First Name:" label-for="input-1">
+              <b-form-input
+                id="input-1"
+                v-model="form.name"
+                placeholder="Enter first name"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Last Name:" label-for="input-2">
+              <b-form-input
+                id="input-2"
+                v-model="form.name"
+                placeholder="Enter last name"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3" label="Address:" label-for="input-3">
+              <b-form-input
+                id="input-3"
+                v-model="form.address"
+                placeholder="Enter address"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-4" label="Balance:" label-for="input-4">
+              <b-form-select
+                id="input-4"
+                v-model="form.balance"
+                required
+              ></b-form-select>
+            </b-form-group>
+
+            <b-button type="Create" variant="primary">Submit</b-button>
+            <b-button type="reset" variant="danger">Reset</b-button>
+          </b-form>
+          <b-card class="mt-3" header="Form Data Result">
+            <pre class="m-0">{{ form }}</pre>
+          </b-card>
+        </div>
+    </b-tab>
     <b-tab title="Workshifts"><p>I'm a disabled tab!</p></b-tab>
   </b-tabs>
 </div>
