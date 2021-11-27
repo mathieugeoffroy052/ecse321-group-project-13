1f3c82c4f08afee8a3297a363bf205ea5e581bf2
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2021-11-26 22:53:55 -0500

    working on login.js

---
+++ LibraryService-Frontend/src/components/login.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+function UserAccountDTO()
+
+
+
+export default {
+    name: 'login',
+    data () {
+        return {
+            response: []
+        }
+    },
+
+    methods: {
+
+
+    }
+
+}
