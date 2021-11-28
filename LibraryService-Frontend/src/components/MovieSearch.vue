2d3c19c19d6f398a6e2f17414bb2a8d52e375ac4
amanij <amani.jammoul@mail.mcgill.ca>
2021-11-28 12:55:27 -0500

    change layout for movie search page

--- LibraryService-Frontend/src/components/MovieSearch.vue
+++ LibraryService-Frontend/src/components/MovieSearch.vue
@@ -1,51 +1,107 @@
 <template>
   <div id="search">
-    <div id="back">
-        <b-button size="sm" pill variant="dark" style="float: left;" @click="redirectToItemSelect()">Back to item type selection</b-button>
-    </div>
     <div class="row">
-      <div class="col-lg-10 center-block">
+      <div class="col">
         <h2>Movies</h2>
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-5">
-        <label for="title" style="float: right;">Search by title: </label>
-      </div>
-      <div class="col">
-        <input type="text" id="requestedTitle" name="title" style="float: left;">
-      </div>
-    </div>
-    
-    <div class="row">
-      <div class="col-5">
-        <label for="director" style="float: right;">Search by director: </label>
-      </div>
-      <div class="col-3">
-        <input type="text" id="requestedDirector" name="director" style="float: left;">
-      </div>
-      <div class="col">
-        <b-button variant="dark" style="float: left;" @click="resetMessages(); runSearch()">Submit</b-button>
-      </div>
-    </div>
+    <b-containter md>
+      <b-col class="shadow p-3 m-3 bg-white rounded">
+        <b-row>
+          <div id="back" style="float: left">
+            <b-button
+              size="sm"
+              pill
+              variant="dark"
+              @click="redirectToItemSelect()"
+              >Back to item type selection</b-button
+            >
+          </div>
+        </b-row>
 
-    <p id="invalidInput"></p>
-    
-    <div>
-      <div id="" style="overflow-y:scroll; height:300px;">
-        <v-row justify="center" v-for="libItem in libraryItems" :key="libItem.name">
-            <input type="radio" name="item" :value="libItem.isbn">{{ " " + libItem.name }}<br> {{ libItem.creator + ", " + libItem.date + ". " + "isbn " + libItem.isbn }} <br>
-        </v-row>
-      </div>
-    </div>
+        <b-row>
+          <b-col cols="5">
+            <label for="title" style="float: right">Search by title: </label>
+          </b-col>
+          <b-col>
+            <input
+              type="text"
+              id="requestedTitle"
+              name="title"
+              style="float: left"
+            />
+          </b-col>
+        </b-row>
 
-    <div id="reserve">
-      <b-button size="lg" pill variant="dark" @click="resetMessages(); createReserveTransaction(7);">Reserve Item</b-button>
-    </div>
+        <b-row>
+          <b-col cols="5">
+            <label for="director" style="float: right"
+              >Search by director:
+            </label>
+          </b-col>
+          <b-col>
+            <input
+              class="mr-2"
+              type="text"
+              id="requestedDirector"
+              name="director"
+              style="float: left"
+            />
+            <b-button
+              variant="dark"
+              style="float: left"
+              @click="
+                resetMessages();
+                runSearch();
+              "
+              >Submit</b-button
+            >
+          </b-col>
+        </b-row>
 
-    <p id="transaction">
-    </p>
+        <p id="invalidInput"></p>
+
+        <div>
+          <div class="shadow p-3 m-3 rounded" id="results" style="overflow-y: scroll; height: 300px">
+            <v-row
+              justify="center"
+              v-for="libItem in libraryItems"
+              :key="libItem.name"
+            >
+              <input type="radio" name="item" :value="libItem.isbn" />{{
+                " " + libItem.name
+              }}<br />
+              {{
+                libItem.creator +
+                ", " +
+                libItem.date +
+                ". " +
+                "isbn " +
+                libItem.isbn
+              }}
+              <br />
+            </v-row>
+          </div>
+        </div>
+
+        <div id="reserve">
+          <br />
+          <b-button
+            size="lg"
+            pill
+            variant="dark"
+            @click="
+              resetMessages();
+              createReserveTransaction(7);
+            "
+            >Reserve Item</b-button
+          >
+        </div>
+
+        <p id="transaction"></p>
+      </b-col>
+    </b-containter>
   </div>
 </template>
 
@@ -53,5 +109,5 @@
 </script>
 
 <style>
-  @import 'styling.css';
+@import "styling.css";
 </style>
