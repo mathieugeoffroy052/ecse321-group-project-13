c0c1479c36f1fb1151343a9531d943c0b0bbd6ae
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2021-11-30 14:26:29 -0500

    added lbirary info page

---
+++ LibraryService-Frontend/src/components/LibraryInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col class="shadow p-3 m-3 bg-white rounded">
+          <h5>Opening Hours</h5>
+        <div class="w-100">
+          <b-table hover :items="allOpeningHours"></b-table>
+        </div>
+      </b-col>
+      <b-col class="shadow p-3 m-3 bg-white rounded">
+        <h5>Holidays</h5>
+        <div class="w-100">
+          <b-table hover :items="allHolidays"></b-table>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script src="./libraryinformation.js"></script>
