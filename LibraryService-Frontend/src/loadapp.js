fd0254b0611e58883b60c9fe7647af1403aaa3c2
amanij <amani.jammoul@mail.mcgill.ca>
2021-12-05 22:37:35 -0500

    triple =

--- LibraryService-Frontend/src/loadapp.js
+++ LibraryService-Frontend/src/loadapp.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 var config = require('../config')
 
 var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = process.env.NODE_ENV == '"production"' ? "https://libraryservice-backend-g13.herokuapp.com" : 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+var backendUrl = process.env.NODE_ENV === 'production' ? "https://libraryservice-backend-g13.herokuapp.com" : 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
